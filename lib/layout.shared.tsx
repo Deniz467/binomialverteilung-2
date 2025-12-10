e745e649421c0826a889760077e50fcafb8d7ba7
Deniz467 <181562052+Deniz467@users.noreply.github.com>
2025-12-10 08:35:07 +0000

    Füge GitHub-URL zu den Basisoptionen hinzu und entferne überflüssige Peer-Abhängigkeiten aus package-lock.json

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,8 +1,10 @@
 import type {BaseLayoutProps} from 'fumadocs-ui/layouts/shared';
 
 export const baseOptions: BaseLayoutProps = {
+  githubUrl: "https://github.com/Deniz467/binomialverteilung-2",
   nav: {
     transparentMode: "always",
     title: 'Binomialverteilung',
   },
+  
 }
